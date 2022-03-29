59156e4ffe9513a61dd20ea67d790f2ce25d3417
AliReza <khazaeinezhadalireza@gmail.com>
2022-03-29 19:14:58 +0430

    بروزرسانی

---
+++ LegoLearn/LegoLearn.Server/Controllers/Home.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace LegoLearn.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Home : ControllerBase
+    {
+       
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+
+    }
+}
