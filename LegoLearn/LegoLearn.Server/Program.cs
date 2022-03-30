ecfafbca3e49763c1d4d11fb1f0047231f6dc423
AliReza <khazaeinezhadalireza@gmail.com>
2022-03-30 21:33:31 +0430

    جلسه سوم

--- LegoLearn/LegoLearn.Server/Program.cs
+++ LegoLearn/LegoLearn.Server/Program.cs
@@ -1,14 +1,29 @@
-using LegoLearn.Server.Services;
+﻿using LegoLearn.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+/// <summary>
+/// چلسه اول
+/// </summary>
+//builder.Services.AddTransient(typeof(ServiceTwo));
+//builder.Services.AddTransient(typeof(IServiceOne), typeof(ServiceOne));
+//builder.Services.AddTransient(typeof(IRepositoryOne<>), typeof(RepositoryOne<>));
 
-builder.Services.AddTransient(typeof(ServiceTwo));
-builder.Services.AddTransient(typeof(IServiceOne), typeof(ServiceOne));
-builder.Services.AddTransient(typeof(IRepositoryOne<>), typeof(RepositoryOne<>));
+/// <summary>
+/// چلسه سوم
+/// </summary>
+builder.Services.Scan(current => current
+    .FromCallingAssembly()
+    .AddClasses()
+
+    .AsSelf()
+    .AsImplementedInterfaces()
+
+    .WithTransientLifetime()
+);
 
 var app = builder.Build();
 app.UseRouting();
